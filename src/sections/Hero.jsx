b392e72bd2366f7455bab79c9d39f6a8b84831d7
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-05-21 01:47:14 -0400

    Hero section basically ready

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,29 +1,25 @@
-import { Download, ArrowRight, Bot } from "lucide-react";
+import { Download, ArrowRight, Bot, ChevronDown } from "lucide-react";
 import { Button } from "@/components/Button";
 import { AnimatedBorderButton } from "../components/AnimatedBorderButton";
+import Marquee from "react-fast-marquee";
 
+const technologies = [
+    "Autodesk Maya", "Substance Painter", "Unity", "C#","Python", "Java", "GitHub", "Git", "Visual Studio"
+]
 const skills = [
-    "Unity",
-    "C#",
-    "C++",
-    "Godot",
-    "GodotScript",
     "Game Design",
     "Level Design",
     "Object Oriented Programming",
-    "Lua",
-    "Python",
-    "Java",
-    "Visual Studio",
-    "Git",
-    "Github",
     "3D Modeling",
     "Basic 3D Animation",
-    "Autodesk Maya",
-    "Public Speaking"
+    "Public Speaking",
+]
+const leisure = [
+    "Watching The Habs", "Farming", "Video Games (duh)", "Hanging Out with Friends", "Coding", "Arts and Crafts", "Reading"
 ]
 
 export const Hero = () => {
+
     return (
     <section className="relative min-h-screen flex items-center overflow-hidden">
         {/* Background */}
@@ -31,7 +27,7 @@ export const Hero = () => {
             <img src="/psp-bg.jpg" alt="Hero image" className="w-full h-full object-cover opacity-40" />
         </div>
 
-        <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background" />
+        <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background" />
         
         {/* Green Dots */}
         <div className="absolute inset-0 overflow-hidden pointer-events-auto">
@@ -104,56 +100,92 @@ export const Hero = () => {
                         
                     </div>
 
-                </div>
-            </div>
+                </div> 
+                
+                {/* Right column - Profile Image */}
+                <div className="space-y-8">
+                    <div className="relative animate-fade-in animation-delay-300">
 
-            {/* Right column - Profile Image */}
-            <div className="relative animate-fade-in animation-delay-300">
-                {/* Profile Image */}
-                <div className="relative max-w-md mx-auto">
-                    <div className="relative glass rounded-3xl p-2 glow-border">
-                        <img src="/dm_profile.jpg" alt="Darcy Mazloum" className="w-full aspect-4/5 object-cover rounded-2xl"/>
+                        {/* Profile Image */}
+                                                        {/*}
+                        <div className="relative max-w-md mx-auto">
+                            <div className="relative glass rounded-3xl p-2 glow-border">
+                                <img src="/dm_profile.jpg" alt="Darcy Mazloum, professional headshot" className="w-full aspect-4/5 object-cover rounded-2xl"/>
+                                                        */}
+                                {/* Floating Badge */}  {/*
+                                <div className="absolute -bottom-5 -right-4 glass rounded-xl px-4 py-3 animate-float">
+                                    <div className="flex items-center gap-3">
+                                        <div className="w-3 h-3 bg-green-500 rounded-full animate-pulse"> </div>
+                                        <span className="text-sm font-medium"> Available For Work </span>
+                                    </div>
+                                </div>                   */}
 
-                        {/* Floating Badge */}
-                        <div className="absolute -bottom-5 -right-4 glass rounded-xl px-4 py-3 animate-float">
-                            <div className="flex items-center gap-3">
-                                <div className="w-3 h-3 bg-green-500 rounded-full animate-pulse"> </div>
-                                <span className="text-sm font-medium"> Available For Work </span>
+                                {/* Stats Badge */}      {/*
+                                <div className="absolute -top-4 -right-4 flass rounded-xl px-4 py-3 animate-float animation-delay-500 bg-card">
+                                    <div className="text-2xl font-bold text-primary">5+</div>
+                                    <div className="text-xs text-muted-foreground">Years Exp.</div>
+                                </div>
+                            </div>
+                        </div>                            */}
+
+                        <div className="overflow-hidden">
+                            <div className="flex animate-marqueeOne">
+                                
+                                <img src="/gigi/at the bar.png" className="w-lg object-cover rounded-2xl" alt="Black and white 2d profile video game, in a bar, main character center and bartender behind bar" />
+                                <div className="px-3"/>
+                                <img src="/mendacity/lab1.png" className="w-lg object-cover rounded-2xl" alt="Strange flesh like creature in a dirty lab"/>
+                                <div className="px-3"/>
+                                <img src="/robohacks23.JPG" className="w-lg object-cover rounded-2xl" alt="Darcy gives a speech at the front of a full classroom"/>
+                                <div className="px-3"/>
+                                <img src="/gigi/gigiFriend.png" className="w-lg object-cover rounded-2xl" alt="Two characters in a black and white 2d game, profile view"/>
+                                <div className="px-3"/>
+                                <img src="/mendacity/baby_scissors.png" className="w-lg object-cover rounded-2xl" alt="Floating white hand in a flesh tunnel, teeth and globs blocking the way"/>
+                                <div className="px-3"/>
+                                <img src="/maze-assignment/keys-maze.png" className="w-lg object-cover rounded-2xl" alt="Three keys, each a different color, each with a number in the center 1, 2, and 3"/>
+
                             </div>
                         </div>
 
-                        {/* Stats Badge */}
-                        <div className="absolute -top-4 -right-4 flass rounded-xl px-4 py-3 animate-float animation-delay-500 bg-card">
-                            <div className="text-2xl font-bold text-primary">5+</div>
-                            <div className="text-xs text-muted-foreground">Years Exp.</div>
-                        </div>
                     </div>
                 </div>
+
             </div>
 
             {/* Skills Section */} 
             <div className="mt-20 animate-fade-in animation-delay-600">
-                <p className="text-sm text-muted-foreground mb-6 text-center"> 
+                <p className="text-sm text-muted-foreground mb-1 text-center"> 
                     Technologies I work with 
                 </p>
                 
                 <div className="relative overflow-hidden">
                     <div className="flex animate-marquee">
-                        {[...skills, ...skills].map((skill,index) =>
+                        {[...technologies, ...technologies].map((tech,index) =>
                         {
-                            skill = " " + skill + " >";
                             return (
-                            <div key={index} className="shrink-0 px8 py-4">
+                            <div key={index} className="shrink-0 px-6 py-2">
                                 <span className="text-xl font-semibold text-muted-foreground/50 hover:text-muted-foreground transition-colors"> 
-                                    {skill} 
+                                    {tech} 
                                 </span>
-                                <span> </span>
                             </div>
                         );})}
                     </div>
                 </div>
             </div>
 
+            
+
+        </div>
+
+        {/* Scoll down chevron */}
+        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-fade-in animation-delay-800">
+            <a
+            href="#about"
+            className="flex flex-col items-center gap-2 text-muted-foreground hover:text-primary">
+                <span className="text-xs uppercase tracking-wider">
+                    Scroll
+                </span>
+                <ChevronDown className="w-6 h-6 animate-bounce"/>
+            </a>
         </div>
 
     </section>
