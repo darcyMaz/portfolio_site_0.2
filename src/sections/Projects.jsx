b2ab24f667e4c826b4dada732c59f86e7a7c90d5
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-05-21 19:31:18 -0400

    Update Projects.jsx
    Started work on projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -53,15 +53,54 @@ export const Projects = () => {
             title: "Sustainable Committee",
             description: " get the actual name please",
             image: "/",
-            tags: [],
+            tags: ["Volunteering", "Community"],
             link: "#",
             github: "#"
         }
     ]
 
     return (
-    <section>
+    <section id="projects" className="py-32 relative overflow-hidden">
         
+        {/* Background Glow */}
+        <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3x1"/> 
+        <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl"/>
+        <div className="container mx-auto px-6 relative z-10"> 
+            {/* Section Header */}
+            <div className="text-center mx-auto max-w-3xl mb-16">
+                <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
+                    Featured Work
+                </span>
+                <h2 className="text-4xl md:text-5xl fo nt-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+                    Projects that
+                    <span className="font-serif italic font-normal text-white"> make an impact. </span>
+                </h2>
+                <p className="text-muted-foreground animate-fade-in animation-delay-400">
+                    Games I've prototyped and experiences I've had that I believe are relevant if you want to get to know me.
+                </p>
+            </div>
+
+            {/* Projects */}
+            <div className="grid md:grid-cols-2 gap-8">
+                {
+                    projects.map((project, idx) => (
+                        <div key={idx} className="group glass rounded-2xl overflow-hidden animate-fade-in md:row-span-1" style={{animationDelay: `${(idx+1) * 100}ms`}}>
+                            
+                            {/* Image */}
+                            <div className="relative overflow-hidden aspect-video">
+                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110" />
+                                <div/>
+                            
+                            </div>
+                            
+
+
+                        </div>
+                    ))
+                }
+            </div>
+
+        </div>
     </section>
     );
 }
