003cf6366ea58063caf8aca02f51f87b20d57be2
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-05-21 17:29:25 -0400

    Started Project section somewhat

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,64 @@
 export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Mendacity",
+            description: " description of the game",
+            image: "/mendacity/mainmenu.png",
+            tags: ["Unity", "C#"],
+            link: "#",
+            github: "#"
+        },
+        {
+            title: "Gigi's Gone Missing... Again.",
+            description: " description of the game",
+            image: "/gigi/mainmenu.png",
+            tags: ["Unity", "C#"],
+            link: "#",
+            github: "#"
+        },
+        {
+            title: "RoboHacks 2023",
+            description: " description of the hackathon",
+            image: "/",
+            tags: ["Leadership", "Event Planning", "Organization", "Public Speaking"],
+            link: "#",
+            github: "#"
+        },
+        {
+            title: "Farming",
+            description: "I'm also a farmer :)",
+            image: "/",
+            tags: ["Fun", "Planting Garlic Cloves", "Manual Labour"],
+            link: "#",
+            github: "#"
+        },
+        {
+            title: "Maze Generator",
+            description: "Generate Random Maze",
+            image: "",
+            tags: ["Unity", "C#", "Algorithm Design", "Search Algorithms"],
+            link: "#",
+            github: "#"
+        },
+        {
+            title: "This website!",
+            description: "based on a tutorial but many changes to suit my needs",
+            image: "a gif of the buttons would be cool or maybe of working on the marquee lol",
+            tags: ["React", "HTML", "CSS", "Javascript", "JS"],
+            link: "#",
+            github: "#"
+        },
+        {
+            title: "Sustainable Committee",
+            description: " get the actual name please",
+            image: "/",
+            tags: [],
+            link: "#",
+            github: "#"
+        }
+    ]
+
     return (
     <section>
         
