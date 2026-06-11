5b437ad382c9118f63e238b5ceb48b09fbb07ba5
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-06-11 16:13:40 -0400

    Update Contacts.jsx

--- src/sections/Contacts.jsx
+++ src/sections/Contacts.jsx
@@ -5,7 +5,7 @@ export const Contacts = () => {
             <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
 
             
-            <div className="absolute"/>
+            <div className=""/>
         </div>
 
 
